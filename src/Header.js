541ebd74be2ebda560be752f91e8f63cac57304b
ÂClaudiaÂPrez88 <perezvelazquezclaudia@gmail.com>
2022-05-31 00:27:10 -0400

    cambios menu

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+const Header = () => {
+return (
+    <header>
+        <nav className="menu">
+            <a href="#">Nosotros</a>
+            <a href="#">Servicios</a>
+            <a href="#">Portafolio</a>
+            <a href="#">Contacto</a>
+        </nav>
+    </header>
+)
+}
+
+export default Header;
