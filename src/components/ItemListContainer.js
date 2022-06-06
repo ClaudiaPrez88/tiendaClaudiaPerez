1003a86e0c46d1384f471844dec0ed9715657a2c
ÂClaudiaÂPrez88 <perezvelazquezclaudia@gmail.com>
2022-06-06 12:55:30 -0400

    Desafío: Crea tu landing

---
+++ src/components/ItemListContainer.js
@@ -0,0 +1,13 @@
+
+
+const ItemListContainer = (prop) => {
+    return(
+        <h1 className="mensaje-slider">Eligue las servilletas que deseas cotizar haciendo click en el botón de compra, luego haz click en el botón de "cotizar mi pedido" y llena el formulario para realizar tu solicitud. Te contactaremos ala brevedad para atender tu solicitud.</h1>
+    )
+};
+
+
+
+export default ItemListContainer;
+
+
