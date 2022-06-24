0f3b6825a353bf5a8c9f8a204aae7f2c326a4a04
ÂClaudiaÂPrez88 <perezvelazquezclaudia@gmail.com>
2022-06-23 23:20:05 -0400

    Corecciones primera devolucion

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -4,37 +4,32 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import customFetch from "../utils/customFetch";
 import ItemList from './ItemList'
 import {Container, Row} from 'react-bootstrap';
-
-
-
+import { useParams } from "react-router-dom";
+import { getProductByCategory } from "../utils/Productos";
 
 
 function ItemListContainer() {
     const [items, Setitems] = useState ([])
-
+    const {categoryId} = useParams()
+    
     useEffect (()=> {
-     
-            customFetch(2000,productos)
-            .then(resultado => Setitems(resultado))
-       
+        if(categoryId)
+         {getProductByCategory(categoryId)
+         .then(response => {Setitems(response)})}
+        else 
+        {customFetch(2000,productos)
+        .then(resultado => Setitems(resultado))
+        }  
     }, [items]);
+    console.log(items)
    
     return (
-        
-            <>
             <Container>
                 <Row>
                     <ItemList productos={items}/>
                 </Row>
-            </Container>
-             
-            </>
-       
-
-    )
-    
-
-    
+            </Container> 
+    )  
 }
 
 
