ce6697ba24d4b0452fc292d2520164db95ff5cfb
ÂClaudiaÂPrez88 <perezvelazquezclaudia@gmail.com>
2022-06-25 20:35:28 -0400

    Correccion primer entrega

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,34 +1,29 @@
 
-import {Navbar, Nav, Container} from 'react-bootstrap';
+import {Navbar, Nav, Container,Col} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Car from './CarWidgets';
 import { Link } from 'react-router-dom';
 
 
+
 const NavBar = () => {
 return (
-    <header>
+      <Col xs={12} md={8}>
         <Navbar className='menu' expand="lg" >
-  <Container>
-    <Navbar.Brand>
-      <Link to='/'>
-        <img src={'img/logo.png'} className="logo" alt='logo'/>
-        </Link>
-      </Navbar.Brand>
-    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="me-auto ">
-        <Link to='category/Campo' >Campo </Link>
-        <Link to='category/Flores' >Flores </Link>
-        <Link to='category/Animales' >Animales</Link>
-        <Nav.Link><Car/></Nav.Link>
-      </Nav>
-    </Navbar.Collapse>
-  </Container>
-</Navbar>
-        
-        
-    </header>
+          <Container>
+            
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto ">
+                <Link to='category/Campo' >Campo </Link>
+                <Link to='category/Flores' >Flores </Link>
+                <Link to='category/Animales' >Animales</Link>
+                <Nav.Link><Car/></Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </Col>  
 )
 }
 
